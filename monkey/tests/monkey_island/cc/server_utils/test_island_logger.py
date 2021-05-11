f8fcb29e9fdc30d6ed3c4357000287a6bd8c352e
Shreya <shreya.malviya@gmail.com>
2021-05-11 18:41:56 +0530

    Modify/add unit tests (FAILS)

--- monkey/tests/monkey_island/cc/server_utils/test_island_logger.py
+++ monkey/tests/monkey_island/cc/server_utils/test_island_logger.py
@@ -6,24 +6,35 @@ import pytest
 from monkey_island.cc.server_utils.island_logger import setup_logging
 
 
-# TODO move into monkey/monkey_island/cc/test_common/fixtures after rebase/backmerge
-@pytest.fixture
-def mock_home_env(monkeypatch, tmpdir):
-    monkeypatch.setenv("HOME", str(tmpdir))
-
-
-def test_expanduser_filename(mock_home_env, tmpdir):
+def test_expanduser_filename_log_level_debug(tmpdir):
     DATA_DIR = tmpdir
     INFO_LOG = os.path.join(DATA_DIR, "monkey_island.log")
-    LOG_LEVEL = "DEBUG"
-    TEST_STRING = "Hello, Monkey!"
+    LOG_LEVEL = logging.DEBUG
+    TEST_STRING = "Hello, Monkey! (Log level: debug)"
 
     setup_logging(DATA_DIR, LOG_LEVEL)
 
     logger = logging.getLogger("TestLogger")
-    logger.info(TEST_STRING)
+    logger.debug(TEST_STRING)
 
     assert os.path.isfile(INFO_LOG)
     with open(INFO_LOG, "r") as f:
         line = f.readline()
         assert TEST_STRING in line
+
+
+def test_expanduser_filename_log_level_info(tmpdir):
+    DATA_DIR = tmpdir
+    INFO_LOG = os.path.join(DATA_DIR, "monkey_island.log")
+    LOG_LEVEL = "INFO"
+    TEST_STRING = "Hello, Monkey! (Log level: info)"
+
+    setup_logging(DATA_DIR, LOG_LEVEL)
+
+    logger = logging.getLogger("TestLogger")
+    logger.debug(TEST_STRING)
+
+    assert os.path.isfile(INFO_LOG)
+    with open(INFO_LOG, "r") as f:
+        line = f.readline()
+        assert TEST_STRING not in line
